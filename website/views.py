5d3466be24733d7cd0d3509bbd02fdd3566fa6b6
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-06-29 09:04:24 +0430

    grammatical edit

--- website/views.py
+++ website/views.py
@@ -4,64 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    x="""
-<head>
-<h1 style='color: #116c91'>
-Main Page
-<h1/>
-<body>
-<h2 style='color: #9820bd'>
-Welcome to Main page
-<h2/>
-<body/>
-<head/>
-"""
-    return HttpResponse(x)
+    return render(request, 'index.html')
 
 
 def page1(request):
-    x="""
-<head>
-<h1 style='color: #116c91'>
-Page 1
-<h1/>
-<body>
-<h2 style='color: #9820bd'>
-Welcome to page 1
-<h2/>
-<body/>
-<head/>
-"""
-    return HttpResponse(x)
+    return render(request, 'a.html')
 
 
 def page2(request):
-    x="""
-<head>
-<h1 style='color: #116c91'>
-Page 2
-<h1/>
-<body>
-<h2 style='color: #9820bd'>
-Welcome to page 2
-<h2/>
-<body/>
-<head/>
-"""
-    return HttpResponse(x)
-
+    return render(request, 'b.html')
 
 def page3(request):
-    x="""
-<head>
-<h1 style='color: #116c91'>
-Page 3
-<h1/>
-<body>
-<h2 style='color: #9820bd'>
-Welcome to page 3
-<h2/>
-<body/>
-<head/>
-"""
-    return HttpResponse(x)
+    return render(request, 'c.html')
