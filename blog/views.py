0f575cfc688bb611bbd9ad0e05b69595f09b5b19
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-10 13:19:17 +0430

    use a filter for database queries

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from blog.models import Post
+from datetime import datetime
 # Create your views here.
 
 def index(request):
-    Posts= Post.objects.filter()
-    Context= {"Posts": Posts}
+    Current_Time = datetime.now()
+    posts= Post.objects.filter(Published_Date__lte= Current_Time)
+    for post in posts:
+        post.Status= True
+    Context= {"posts": posts}
     return render(request, 'blog/blog-home.html', Context)
 
 
 def single(request):
+    def Add_View(post):
+        post.Counted_view += 1
+    # Because of we do not have any auther yet, we can not use Add_View
     return render(request, 'blog/blog-single.html')
