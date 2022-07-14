4b9877b75f43fba04c8364db4e0e7796809eb507
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-14 12:58:16 +0430

    Create Category model
    Create Category model and add a manytomany field to Post model which is related to category model.
    
    migrate the datebase

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    Name= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Name
+
 class Post(models.Model):
     Image= models.ImageField(upload_to = 'blog/', default= 'blog/Default.png')
     Author= models.ForeignKey(User, on_delete= models.SET_NULL, null= True)
     Title= models.CharField(max_length= 255)
     Content= models.TextField()
     # tag
-    # category
+    Category= models.ManyToManyField(Category)
     Counted_view= models.IntegerField(default= 0)
     Status= models.BooleanField(default= False)
     Published_Date= models.DateTimeField(null= True)
