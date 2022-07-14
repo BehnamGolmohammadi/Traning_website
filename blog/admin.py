d4fc173957b3e61283703a5c458d764ef5b5bf68
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-14 08:13:43 +0430

    Add author as a foreignkey to Post model
    use author field as a foreign key
    it will get its attributes from User model and it will use in admin page for showing and filtering results.

--- blog/admin.py
+++ blog/admin.py
@@ -8,13 +8,13 @@ class Post_Admin_Customization(admin.ModelAdmin):
     empty_value_display= 'No Information'
     fieldsets = (
         ('Main options', {
-            'fields': ("Title", "Content")
+            'fields': ("Author", "Title", "Content")
         }),
         ('Advanced options', {
             'classes': ('wide', 'collapse', 'extrapretty'),
             'fields': ("Status", "Counted_view", "Published_Date", ),
         }),
     )
-    list_display= ("Title", "Content" ,"Status", "Counted_view", "Published_Date")
-    list_filter= ("Status", "Published_Date")
-    search_fields= ["Title", "Content"]
+    list_display= ("Title", "Author" ,"Status", "Counted_view", "Published_Date")
+    list_filter= ("Status", "Author", "Published_Date")
+    search_fields= ["Title", "Author", "Content"]
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     # Image
-    # auther
+    Author= models.ForeignKey(User, on_delete= models.SET_NULL, null= True)
     Title= models.CharField(max_length= 255)
     Content= models.TextField()
     # tag
