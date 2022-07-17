d0c7ab03e5be48c74ec333cef2b338e0e2eebc21
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-17 15:02:45 +0430

    Create inclusion tag: Post_Categories
    make custom tag to show frequency of use of categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,7 +2,7 @@ from django import template
 from django.utils import timezone
 
 
-from blog.models import Post
+from blog.models import Category, Post
 
 register= template.Library()
 
@@ -29,3 +29,14 @@ def Past_Time(Published_Date):
     past_time=  Current_Time - Published_Date
     past_days= past_time.days
     return str(past_days) + ' Days ago'
+    
+@register.inclusion_tag('blog/blog-PostCategories.html')
+def Post_Categories():
+    posts= Post.objects.filter(Status= True)
+    categories= Category.objects.all()
+    Category_Dict= dict()
+    
+    for category in categories:
+        Category_Dict[category]= posts.filter(Category= category).count()
+
+    return {'Category_Dict': Category_Dict}
