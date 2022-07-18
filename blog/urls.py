cb2614b6f8c02cfefdbe35ee1f5632b0badbafc8
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-18 09:38:39 +0430

    add author path to url
    its will point to index view

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', blog_index, name='index'),
     path('<int:pid>/', blog_single, name='single'),
     path('category/<str:cat_name>/', blog_index, name='category'),
+    path('author/<str:author_username>/', blog_index, name='author'),
 ]
