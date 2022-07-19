c54848c9aee6b8b910d2365423aa7f9a655891d0
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-19 11:12:30 +0430

    Add recent posts of blog page to home page
    1. create custom tag folder with templatetags name
    2. add __init__ file to templatetags
    3. create a new to file to add register new custom tags with website_tags.py name
    4. move latest post field from index.html of home page to specific html file and complete it with website_tags

---
+++ website/templatetags/website_tags.py
@@ -0,0 +1,21 @@
+from django import template
+register= template.Library()
+from blog.models import Post, Category
+
+
+@register.simple_tag
+def Latestpost():
+    posts= Post.objects.filter(Status= True).order_by('Published_Date')[:6]
+    Context= {'posts': posts}
+    return Context
+
+
+@register.filter
+def Short_Content(Content,Len= 35):
+    return ' '.join(Content.split()[:Len]) + ' ...'
+
+@register.inclusion_tag('website/home-Recentblogposts.html')
+def Latestposts():
+    posts= Post.objects.filter(Status= True).order_by('-Published_Date')[:6]
+    Context= {'posts': posts}
+    return Context
