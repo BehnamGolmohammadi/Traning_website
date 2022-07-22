e743c907ff188b79109385a424fcaff1441a7133
BehnamGolmohammadi <99076800+BehnamGolmohammadi@users.noreply.github.com>
2022-07-22 14:09:12 +0430

    add new path for news letter

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, about, contact
+from .views import *
 app_name= 'website'
 
 urlpatterns = [
     path('', index, name= 'index'),
     path('about', about, name= 'about'),
     path('contact', contact, name= 'contact'),
+    path('newsletter', newsletter, name= 'newsletter')
     ]
